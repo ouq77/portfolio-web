4a4b4217373e1ab31824ed61719aa81ab0b85036
Louw Swart <louw77@gmail.com>
2017-07-02 11:21:28 +1200

    Additional logging for 404 pages

--- server/routes/fourohfour.js
+++ server/routes/fourohfour.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = (req, res) => {
-  console.error('No resource matches: ' + req.url)
+  console.error(`No resource matches: ${req.url} [${req.headers['user-agent']}]`)
   res.status(404)
     .sendFile(path.resolve(__dirname, '../../app/404.html'))
 }
--- server/routes/reportviolation.js
+++ server/routes/reportviolation.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
   if (req.body) {
-    console.log('CSP Violation: ', req.body)
+    console.log(`CSP Violation: ${req.body}`)
   } else {
     console.log('CSP Violation: No data received!')
   }
