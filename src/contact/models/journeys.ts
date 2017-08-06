559ce9bc2199853b42c7a8182785f3f930971c81
Louw Swart <louw77@gmail.com>
2017-08-07 09:01:20 +1200

    Updated dependencies and journeys

--- src/contact/models/journeys.ts
+++ src/contact/models/journeys.ts
@@ -29,7 +29,7 @@ export const JOURNEYS: Array<Array<IAirport>> = [
   [airports.LHR, airports.ARN], [airports.BGO, airports.TRD], [airports.BOO, airports.SJV], [airports.TOS, airports.ARN],
   [airports.LHR, airports.SIN, airports.SYD, airports.WLG], [airports.WLG, airports.TIU], [airports.WLG, airports.NSN],
   [airports.WLG, airports.NPL], [airports.WLG, airports.CBR, airports.SIN, airports.JNB], [airports.WLG, airports.ZQN],
-  [airports.BKKN, airports.KBV], [airports.HKT, airports.SIN, airports.MEL, airports.WLG],
+  [airports.BKKN, airports.KBV], [airports.HKT, airports.SIN, airports.MEL, airports.WLG], [airports.WLG, airports.GIS],
 ];
 
 /**
@@ -37,5 +37,5 @@ export const JOURNEYS: Array<Array<IAirport>> = [
  * @type {IAirport[][]}
  */
 export const UPCOMING_JOURNEYS: Array<Array<IAirport>> = [
-  [airports.WLG, airports.GIS], [airports.AKL, airports.YVR],
+  [airports.AKL, airports.YVR],
 ];
