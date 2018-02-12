db1a2109b4a402c0cafb4f75fe6c9cfa4f17b6c4
Louw Swart <louw@ouq77.kiwi>
2018-02-12 20:56:54 +1300

    Further Lighthouse experiments

--- src/sw.js
+++ src/sw.js
@@ -40,35 +40,38 @@ function createCacheBustedRequest(url) {
   return new Request(bustedUrl);
 }
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    // We can't use cache.add() here, since we want OFFLINE_URL to be the cache key, but
-    // the actual URL we end up requesting might include a cache-busting parameter.
-    fetch(createCacheBustedRequest(OFFLINE_URL)).then(function(response) {
-      return caches.open(CURRENT_CACHES.offline).then(function(cache) {
+self.addEventListener('install', function (event) {
+  // We can't use cache.add() here, since we want OFFLINE_URL to be the cache key, but
+  // the actual URL we end up requesting might include a cache-busting parameter.
+  var offLinePromise = fetch(createCacheBustedRequest(OFFLINE_URL))
+    .then(function (response) {
+      return caches.open(CURRENT_CACHES.offline).then(function (cache) {
         return cache.put(OFFLINE_URL, response);
       });
-    })
-  );
-  event.waitUntil(caches.open(CURRENT_CACHES.regular)
-    .then(function(cache) {
+    });
+
+  var regularPromise = caches.open(CURRENT_CACHES.regular)
+    .then(function (cache) {
       return cache.addAll(REGULAR_URLS);
-    })
+    });
+
+  event.waitUntil(
+    Promise.all([offLinePromise, regularPromise])
   );
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
   // Delete all caches that aren't named in CURRENT_CACHES.
   // While there is only one cache in this example, the same logic will handle the case where
   // there are multiple versioned caches.
-  var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
+  var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function (key) {
     return CURRENT_CACHES[key];
   });
 
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
+        cacheNames.map(function (cacheName) {
           if (expectedCacheNames.indexOf(cacheName) === -1) {
             // If this cache name isn't present in the array of "expected" cache names, then delete it.
             console.log('Deleting out of date cache:', cacheName);
@@ -80,57 +83,24 @@ self.addEventListener('activate', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   // We only want to call event.respondWith() if this is a navigation request
   // for an HTML page.
   // request.mode of 'navigate' is unfortunately not supported in Chrome
   // versions older than 49, so we need to include a less precise fallback,
   // which checks for a GET request with an Accept: text/html header.
   if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+    console.log('Handling fetch event for', event.request.url);
     event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
-
-          // IMPORTANT: Clone the request. A request is a stream and
-          // can only be consumed once. Since we are consuming this
-          // once by cache and once by the browser for fetch, we need
-          // to clone the response.
-          var fetchRequest = event.request.clone();
-
-          return fetch(fetchRequest).then(
-            function(response) {
-              // Check if we received a valid response
-              if(!response || response.status !== 200 || response.type !== 'basic') {
-                return response;
-              }
-
-              // IMPORTANT: Clone the response. A response is a stream
-              // and because we want the browser to consume the response
-              // as well as the cache consuming the response, we need
-              // to clone it so we have two streams.
-              var responseToCache = response.clone();
-
-              caches.open(CURRENT_CACHES.regular)
-                .then(function(cache) {
-                  cache.put(event.request, responseToCache);
-                });
-
-              return response;
-            }
-          ).catch(function(error) {
-            // The catch is only triggered if fetch() throws an exception, which will most likely
-            // happen due to the server being unreachable.
-            // If fetch() returns a valid HTTP response with an response code in the 4xx or 5xx
-            // range, the catch() will NOT be called. If you need custom handling for 4xx or 5xx
-            // errors, see https://github.com/GoogleChrome/samples/tree/gh-pages/service-worker/fallback-response
-            console.log('Fetch failed; returning offline page instead.', error);
-            return caches.match(OFFLINE_URL);
-          });
-        })
+      fetch(event.request).catch(function(error) {
+        // The catch is only triggered if fetch() throws an exception, which will most likely
+        // happen due to the server being unreachable.
+        // If fetch() returns a valid HTTP response with an response code in the 4xx or 5xx
+        // range, the catch() will NOT be called. If you need custom handling for 4xx or 5xx
+        // errors, see https://github.com/GoogleChrome/samples/tree/gh-pages/service-worker/fallback-response
+        console.log('Fetch failed; returning offline page instead.', error);
+        return caches.match(OFFLINE_URL);
+      })
     );
   }
 
