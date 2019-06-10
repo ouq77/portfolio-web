280bc60eb7d69038470d28ea7194f5bd60f2b016
Louw Swart <louw.swart@dia.govt.nz>
2019-06-10 12:40:35 +1200

    Revert assets route

--- server/routes/assets.js
+++ server/routes/assets.js
@@ -2,6 +2,6 @@ const serveWebp = require('serve-webp')
 const path = require('path')
 
 module.exports = serveWebp({
-  root: path.resolve(__dirname, '../../app/assets/images'),
+  root: path.resolve(__dirname, '../../app'),
   cache: path.resolve(__dirname, '../../app/assets', 'webp')
 })
