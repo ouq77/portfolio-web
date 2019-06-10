987c23d3c39e3791eb3349e823307fb02427e4c5
Louw Swart <louw.swart@dia.govt.nz>
2019-06-10 12:22:37 +1200

    Release version: 8.1.4

--- server/routes/assets.js
+++ server/routes/assets.js
@@ -2,6 +2,6 @@ const serveWebp = require('serve-webp')
 const path = require('path')
 
 module.exports = serveWebp({
-  root: path.resolve(__dirname, '../../app'),
+  root: path.resolve(__dirname, '../../app/assets/images'),
   cache: path.resolve(__dirname, '../../app/assets', 'webp')
 })
