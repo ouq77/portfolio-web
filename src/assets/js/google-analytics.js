d589d2fafa844cbe8e639e3f5d741395648f2154
Louw Swart <louw.swart@dia.govt.nz>
2019-06-10 18:30:40 +1200

    Release version: 8.1.9

--- src/assets/js/google-analytics.js
+++ src/assets/js/google-analytics.js
@@ -6,10 +6,10 @@ ga('create', '<%= GOOGLE_ACCOUNT.analytics %>', 'auto');
 ga('send', 'pageview');
 
 (function($) {
-  $('.js_track_link_click').on('click', function() {
+  $(document).on('click', '.js_track_link_click', function() {
     ga('send', 'event', 'link_click', $(this).attr('href'));
   });
-  $('.btn-submit').on('click', function() {
+  $(document).on('click', '.btn-submit', function() {
     ga('send', 'event', 'button_click', $(this).attr('value'));
   });
 })(jQuery);
