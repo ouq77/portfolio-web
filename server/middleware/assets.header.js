aa3a477a3eb0a6ff9d1be1678399ee0f4e5bb317
Louw Swart <louw.swart@dia.govt.nz>
2019-06-10 17:45:29 +1200

    Test UA

--- server/middleware/assets.header.js
+++ server/middleware/assets.header.js
@@ -1,4 +1,9 @@
+const parser = require('ua-parser-js')
+
 module.exports = (req, res, next) => {
+  const ua = parser(req.headers['user-agent'])
+  const supported = ua.browser.name === 'Chrome' && ua.browser.major >= '23'
+  console.log('>>>>>> Supports webp', supported)
   res.header('Vary', 'User-Agent, Accept')
   res.header('Cache-Control', 'public, no-cache, proxy-revalidate')
   next()
