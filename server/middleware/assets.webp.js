6ec45dd447bb89161dc495aa66002a3214c2572f
Louw Swart <louw.swart@dia.govt.nz>
2019-06-16 11:59:36 +1200

    Leave quality at default

--- server/middleware/assets.webp.js
+++ server/middleware/assets.webp.js
@@ -2,6 +2,5 @@ const webp = require('webp-middleware')
 const path = require('path')
 
 module.exports = webp(path.resolve(__dirname, '../../app'), {
-  cacheDir: path.resolve(__dirname, '../../app/assets/webp', 'cache'),
-  quality: 100
+  cacheDir: path.resolve(__dirname, '../../app/assets/webp', 'cache')
 })
