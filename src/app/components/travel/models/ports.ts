4f0c507a3e06b962ad643bc03174c2dedff235d5
Louw Swart <louw@ouq77.kiwi>
2020-08-10 18:39:08 +1200

    Release version: 10.0.8

--- src/app/components/travel/models/ports.ts
+++ src/app/components/travel/models/ports.ts
@@ -1,6 +1,10 @@
 import { Port } from '../definitions/port'
-import { DENMARK, NEW_CALEDONIA, NEW_ZEALAND, NORWAY } from './countries'
+import { DENMARK, NEW_CALEDONIA, NEW_ZEALAND, NORWAY, UK } from './countries'
 
+export const GBDVR: Port = { city: 'Dover', country: UK, portCode: 'GBDVR', loc: { lat: 51.124770, lng: 1.332870 }, name: 'Port of Dover' }
+export const FRCQF: Port = { city: 'Calais', country: 'France', portCode: 'FRCQF', loc: { lat: 50.966640, lng: 1.868510 }, name: 'Port of Calais' }
+export const NZWLG: Port = { city: 'Wellington', country: NEW_ZEALAND, portCode: 'NZPCN', loc: { lat: -41.279930, lng: 174.781710 }, name: 'Port of Wellington' }
+export const NZPCN: Port = { city: 'Picton', country: NEW_ZEALAND, portCode: 'NZPCN', loc: { lat: -41.285700, lng: 174.005110 }, name: 'Port of Picton' }
 export const DKCPH: Port = { city: 'København', country: DENMARK, portCode: 'DKCPH', loc: { lat: 55.702022, lng: 12.595930 }, name: 'Port of Copenhagen' }
 export const NOOSL: Port = { city: 'Oslo', country: NORWAY, portCode: 'NOOSL', loc: { lat: 59.902835, lng: 10.744473 }, name: 'Port of Oslo' }
 export const NOSVJ: Port = { city: 'Svolvær', country: NORWAY, portCode: 'NOSVJ', loc: { lat: 68.230790, lng: 14.566888 }, name: 'Port of Svolvær' }
@@ -15,5 +19,5 @@ export const NCMEE: Port = { city: 'Mare', country: NEW_CALEDONIA, portCode: 'NC
  * Array of all ports for the Contact section
  */
 export const PORTS: Array<Port> = [
-  DKCPH, NOOSL, NOSVJ, NOTOS, NZAKL, NCNOU, LIFOU, VUVLI, NCMEE,
+  GBDVR, FRCQF, DKCPH, NOOSL, NOSVJ, NOTOS, NZAKL, NZPCN, NZWLG, NCNOU, LIFOU, VUVLI, NCMEE,
 ]
